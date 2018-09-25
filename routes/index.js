544e34462667fbd827c4976896653d3961b7bfa6
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2018-09-25 16:16:41 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@ var express = require('express')
 var router = express.Router()
 // const request = require('request')
 const version = require('../package.json').version
-var authMiddleware = new AuthMiddleware()
 const request = require('request')
 
 // START OF HOMEPAGE
 router.get('/', function (req, res, next) {
-  res.render("yo")
+  res.render('login')
 })
 
 module.exports = router
