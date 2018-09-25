3162124703da36ec460635809595019b813e3e0e
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2018-09-25 15:25:01 -0400

    init package

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var cons = require('consolidate')
+var index = require('./routes/index')
+const version = require('./package.json').version
+var app = express()
+
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // render the error page
+  res.status(err.status || 500)
+  res.render('err')
+  console.log(err)
+})
+
+module.exports = app
