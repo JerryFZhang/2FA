84301b8675af1d30381b06fda54be046bae943ce
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2018-10-02 19:40:13 -0400

    removed unused schema

--- models/user.js
+++ models/user.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 const Client = require('authy-client').Client;
-
 const authy = new Client({
     key: "h7GPkzc0gB5ao08jwGzVqzHeZcJPRMoo"
 });
 const twilioClient = require('twilio')('AC40fff50dab966d6478fe8307185e1bc0', 'ae552f14a1bd2442de49a8310fa975c6');
-
-
 var UserSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -38,46 +35,14 @@ var UserSchema = new mongoose.Schema({
     }
 })
 
-// authenticate input against database
-UserSchema.statics.authenticate = function (username, password, callback) {
-    User.findOne({
-        username: username,
-    }, function (err, user) {
-        if (err) {
-            return callback(err)
-        } else if (!user) {
-            err = new Error('User not found.')
-            err.status = 401
-            return callback(err)
-        }
-        bcrypt.compare(password, user.password, function (err, result) {
-            console.log(password)
-            console.log(user.password)
-            console.log(result)
-
-            if (err) {
-                return callback(err)
-                console.log("noooo")
-            }
-            if (result === true) {
-                console.log("yay")
-                return callback(null, user)
-            } else {
-                console.log("ayyayays")
-                return callback()
-            }
-        })
-    })
-}
 UserSchema.statics.verify = function (token, password, username, callback) {
     User.findOne({
-        username: username
+        username: "123"
     }).exec(function (err, user) {
         console.log("Verify Token");
-        console.log(user)
         if (err) {
             console.error('Verify Token User Error: ', err);
-            res.status(500).json(err);
+            return callback(err)
         }
         authy.verifyToken({
             authyId: user.authyId,
@@ -85,28 +50,24 @@ UserSchema.statics.verify = function (token, password, username, callback) {
         }, function (err, tokenRes) {
             if (err) {
                 console.log("Verify Token Error: ", err);
-                res.status(500).json(err);
-                return;
+                return callback(err)
             }
             console.log("Verify Token Response: ", tokenRes);
-            if (tokenRes.success) {
+            if (tokenRes.success ==true) {
                 req.session.authy = true;
+                return callback(true)
             }
-            res.status(200).json(tokenRes);
         });
+        return callback(false)
     });
 };
-
 UserSchema.statics.sms = function (username, callback) {
     User.findOne({
         username: username,
     }, function (err, user) {
-        console.log("Send SMS");
-        console.log(user);
-        if (err) {
+        if (err || !user) {
             console.log('SendSMS', err);
-            res.status(500).json(err);
-            return;
+            return callback
         }
         authy.requestSms({
             authyId: user.authyId
@@ -115,13 +76,12 @@ UserSchema.statics.sms = function (username, callback) {
         }, function (err, smsRes) {
             if (err) {
                 console.log('ERROR requestSms', err);
-                res.status(500).json(err);
-                return;
+                return err
             }
             console.log("requestSMS response: ", smsRes);
-            res.status(200).json(smsRes);
+            if (smsRes.success == true)return true
         });
-
+        return false
     });
 };
 
@@ -146,7 +106,6 @@ UserSchema.statics.resetPassword = function (username, password, callback) {
         })
     })
 }
-
 // hashing a password before saving it to the database
 UserSchema.pre('save', function (next) {
     var user = this
@@ -160,23 +119,6 @@ UserSchema.pre('save', function (next) {
         next()
     })
 })
-
-
-// Send a text message via twilio to this user
-UserSchema.methods.sendMessage =
-    function (message, successCallback, errorCallback) {
-        const self = this;
-        const toNumber = `+${self.countryCode}${self.phone}`;
-
-        twilioClient.messages.create({
-            to: toNumber,
-            body: message,
-        }).then(function () {
-            successCallback();
-        }).catch(function (err) {
-            errorCallback(err);
-        });
-    };
 
 const User = mongoose.model('user', UserSchema)
 module.exports = User
