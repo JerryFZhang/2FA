3d0e82e053fb6b925a972c6d49b0c722547583b4
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2018-10-01 23:05:08 -0400

    err

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use(function (err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {}
     // render the error page
     res.status(err.status || 500)
-    res.render('err')
+    res.send('err')
     console.log(err)
 })
 // http.createServer(lex.middleware(redirectHttps())).listen(80);
